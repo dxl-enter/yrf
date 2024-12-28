a5e53214fa351d4f2cbb93292acebb5fbbb1d681
dxl-enter <1579486716@qq.com>
2024-12-28 16:52:02 +0800

    Update analysis-team.tsx

--- src/pages/dashboard/workbench/analysis-team.tsx
+++ src/pages/dashboard/workbench/analysis-team.tsx
@@ -1,13 +1,30 @@
-type Props = {
-	subtitle: string;
-	title: string;
-};
-
-export default function AnalysisTeam({ subtitle, title }: Props) {
+import {Card, Splitter} from "antd";
+import React from "react";
+interface CardOperateProps {
+    operations: Array<{ title: string; subtitle: string; onClick: () => void }>;
+}
+const AnalysisTeam: React.FC<CardOperateProps> = ({ operations = [] }) => {
 	return (
-		<div className="flex flex-col items-center justify-between">
-			<span className="text-xl font-bold">{title}</span>
-			<span className="text-sm">{subtitle}</span>
-		</div>
+		<Card className="flex-col !p-0" styles={{
+            body: {
+                display: "flex",
+                alignItems: "center",
+                padding: "12px 0",
+            },
+        }}>
+            <Splitter>
+                {operations.map((operation) => (
+                    <Splitter.Panel collapsible key={operation.title}>
+                        <div className="flex flex-col items-center justify-between" onClick={operation.onClick}>
+							<span className="text-xl font-bold">{operation.title}</span>
+							<span className="text-sm">{operation.subtitle}</span>
+						</div>
+                    </Splitter.Panel>
+                ))}
+            </Splitter>
+        </Card>
+		
 	);
 }
+
+export default AnalysisTeam;
