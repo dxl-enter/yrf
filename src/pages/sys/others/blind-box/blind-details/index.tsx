641c58a0edccdafd6228234b13aca9230e4e1f48
dxl-enter <1579486716@qq.com>
2024-12-28 17:11:58 +0800

    Update index.tsx

--- src/pages/sys/others/blind-box/blind-details/index.tsx
+++ src/pages/sys/others/blind-box/blind-details/index.tsx
@@ -6,10 +6,11 @@ import { useState } from "react";
 import BlindBoxContent from "./components/blind-box-content";
 import GameplayIntroduction from "./components/gameplay-introduction";
 import { useTranslation } from "react-i18next";
-import {Button, Tag} from "antd";
+import {Button, Tag, Typography} from "antd";
 import React from "react";
 import FooterSecondary from "@/layouts/_common/footer-secondary";
 import {useNavigate} from "react-router";
+
 
 function UserProfile() {
 	const navigatge = useNavigate();
@@ -77,6 +78,7 @@ function UserProfile() {
 			</Card>
 			<div>{tabs[currentTabIndex].content}</div>
 			<FooterSecondary>
+				<Typography.Title style={{float: 'left', marginBottom: 0}} level={4}>124USDT</Typography.Title>
 				<Button
 					type="primary"
 					style={{ float: 'right' }}
